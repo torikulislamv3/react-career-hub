b827ea4c4f258a29c667fa1d1b7e368f4d6259fb
torikulislamv3 <tamim.officialinfo7@gmail.com>
2024-03-25 12:12:47 +0600

    root added, footer added, outlet added

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+
 import './index.css'
 
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import Root from './component/root/Root';
+import Home from './component/home/Home';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Setup for Career Hub!</div>,
+    element: <Root></Root>,
+    children:[
+      {
+        path: "/",
+        element: <Home></Home>
+      },
+      
+    ]
   },
 ]);
 
